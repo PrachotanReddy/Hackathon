5d39524c6ad507ae5c57af45046b8e17a2f9a1e8
Yash Aryan <yash.aryan1@learner.manipal.edu>
2020-09-03 13:45:34 +0530

    UI changes

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,37 +1,53 @@
-const destinations = ["announcement", "welcome", "teamconfirm", "problem", "submission"]
+const destinations = [
+  'announcement',
+  'welcome',
+  'teamconfirm',
+  'problem',
+  'submission',
+]
+
+function loadingProcess() {
+  changeTab('welcome-toggle')
+  removeLoader()
+}
+
+function removeLoader() {
+  document.getElementById('loader').style.display = 'none'
+  document.getElementById('main').style.display = 'flex'
+}
 
 function changeTab(id) {
-    let redirectDestination = id.slice(0, -7);
-    document.getElementById("announcement-toggle").classList.remove("active");
-    document.getElementById("welcome-toggle").classList.remove("active");
-    document.getElementById("teamconfirm-toggle").classList.remove("active");
-    document.getElementById("problem-toggle").classList.remove("active");
-    document.getElementById("submission-toggle").classList.remove("active");
-    if (redirectDestination === "announcement") {
-        renderAnnouncements();
+  let redirectDestination = id.slice(0, -7)
+  document.getElementById('announcement-toggle').classList.remove('active')
+  document.getElementById('welcome-toggle').classList.remove('active')
+  document.getElementById('teamconfirm-toggle').classList.remove('active')
+  document.getElementById('problem-toggle').classList.remove('active')
+  document.getElementById('submission-toggle').classList.remove('active')
+  if (redirectDestination === 'announcement') {
+    renderAnnouncements()
+  }
+  for (let i = 0; i < destinations.length; i++) {
+    if (destinations[i] == redirectDestination) {
+      let sidebarLinkSelection = destinations[i] + '-toggle'
+      document.getElementById(`${sidebarLinkSelection}`).classList.add('active')
+      document.getElementById(`${destinations[i]}`).style.display = 'flex'
+    } else {
+      document.getElementById(`${destinations[i]}`).style.display = 'none'
     }
-    for (let i = 0; i < destinations.length; i++) {
-        if (destinations[i] == redirectDestination) {
-            let sidebarLinkSelection = destinations[i] + "-toggle";
-            document.getElementById(`${sidebarLinkSelection}`).classList.add("active")
-            document.getElementById(`${destinations[i]}`).style.display = "flex";
-        } else {
-            document.getElementById(`${destinations[i]}`).style.display = "none";
-        }
-    }
+  }
 }
 
 function renderAnnouncements() {
-    let announceContent = "";
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let recievedData = JSON.parse(this.responseText);
-            for (let i = recievedData.length - 1; i >= 0; i--) {
-                let news = recievedData[`${i}`].para
-                let heading = recievedData[`${i}`].announcementTitle
-                let announceDate = recievedData[`${i}`].dateText
-                let announceContent = `<div class="card">
+  let announceContent = ''
+  var xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let recievedData = JSON.parse(this.responseText)
+      for (let i = recievedData.length - 1; i >= 0; i--) {
+        let news = recievedData[`${i}`].para
+        let heading = recievedData[`${i}`].announcementTitle
+        let announceDate = recievedData[`${i}`].dateText
+        let announceContent = `<div class="card">
                     <h2 class="card-title">
                         ${heading}
                     </h2>
@@ -42,10 +58,10 @@ function renderAnnouncements() {
                         <p>${announceDate}</p>
                     </div>
                     </div>`
-                document.getElementById("container-holder").innerHTML += announceContent;
-            }
-        }
-    };
-    xhttp.open("GET", "/getNews", true);
-    xhttp.send();
+        document.getElementById('container-holder').innerHTML += announceContent
+      }
+    }
+  }
+  xhttp.open('GET', '/getNews', true)
+  xhttp.send()
 }
--- public/scripts/get-started.js
+++ public/scripts/get-started.js
@@ -1,34 +1,40 @@
 window.onload = function () {
-      Particles.init({
-      selector: '.background-particle',
-      color: '#ffffff',
-      connectParticles: false,
-      retina_detect: true,
-      });
-      loadingFunction();
-};
+  Particles.init({
+    selector: '.background-particle',
+    color: '#ffffff',
+    connectParticles: false,
+    retina_detect: true,
+  })
+  loadingFunction()
+  removeLoader()
+}
 function loadingFunction() {
-      let redirect = window.location.href.split('#');
-      if(redirect == 'login') {
-            document.getElementById('registerButton').click();
-      }
-      else {
-            document.getElementById('loginButton').click();
-      }
+  let redirect = window.location.href.split('#')
+  if (redirect[1] == 'register') {
+    document.getElementById('registerButton').click()
+  } else {
+    document.getElementById('loginButton').click()
+  }
 }
 function changeToLogin(item) {
-      document.getElementById('login-container').style.display='flex';
-      document.getElementById('signup-container').style.display='none';
-      item.style.backgroundColor = "#fff";
-      item.style.color = "#000";
-      document.getElementById('registerButton').style.color = "#fff";
-      document.getElementById('registerButton').style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  document.getElementById('login-container').style.display = 'flex'
+  document.getElementById('signup-container').style.display = 'none'
+  item.style.backgroundColor = '#fff'
+  item.style.color = '#000'
+  document.getElementById('registerButton').style.color = '#fff'
+  document.getElementById('registerButton').style.backgroundColor =
+    'rgba(0, 0, 0, 0.7)'
 }
 function changeToRegister(item) {
-      document.getElementById('login-container').style.display='none';
-      document.getElementById('signup-container').style.display='flex';
-      item.style.backgroundColor = "#fff";
-      item.style.color = "#000";
-      document.getElementById('loginButton').style.color = "#fff" ;
-      document.getElementById('loginButton').style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  document.getElementById('login-container').style.display = 'none'
+  document.getElementById('signup-container').style.display = 'flex'
+  item.style.backgroundColor = '#fff'
+  item.style.color = '#000'
+  document.getElementById('loginButton').style.color = '#fff'
+  document.getElementById('loginButton').style.backgroundColor =
+    'rgba(0, 0, 0, 0.7)'
+}
+
+function removeLoader() {
+  document.getElementById('loader').style.display = 'none'
 }
